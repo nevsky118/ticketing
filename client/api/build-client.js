e842be9d5a47b1513a77636650e2f7491e09be26
Vadim <25124427+nevsky118@users.noreply.github.com>
2021-07-16 21:22:38 +0300

    Update build-client.js

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
 		return axios.create({
 			baseURL:
-				'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+				'http://arendaphone.email/',
 			headers: req.headers,
 		});
 	} else {
